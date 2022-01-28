27edd005ae3d17acae8d7ef0df32e51dc297e6d1
Mikael Viklund <mikael.viklund@bolagsverket.se>
2022-01-28 13:57:39 +0100

    History sharing

--- 090_history.bash
+++ 090_history.bash
@@ -1,5 +1,9 @@
 # History
 export HISTTIMEFORMAT="%h %d %H:%M:%S "
+export HISTCONTROL=ignoredups:erasedups  # no duplicate entries
+export HISTSIZE=100000                   # big big history
+export HISTFILESIZE=100000               # big big history
 shopt -s histappend
-PROMPT_COMMAND='history -a'
-export HISTCONTROL=ignorespace:erasedups
+#PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a; history -c; history -r"
+PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND}history -c;history -r"
+

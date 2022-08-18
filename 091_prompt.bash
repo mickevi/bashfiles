b6e050031e9dae7b8c43ffc3e4e0b7a2dca7f89f
Mikael Viklund <mikael.viklund@bolagsverket.se>
2022-08-18 12:43:20 +0200

    collors and comments.

--- 091_prompt.bash
+++ 091_prompt.bash
@@ -8,3 +8,10 @@ GIT_PROMPT_START='\[\e[0m\]#\[\e[0;97m\][\[\e[0;96m\]\u\[\e[0;95m\]@\[\e[0;96m\]
 GIT_PROMPT_END='-[\[\e[0m\]$?\[\e[0;97m\]]\n\[\e[0m\]#\[\e[0m\]\w\[\e[0m\]:\[\e[0m\] '
 source ~/.bash-git-prompt/gitprompt.sh
 
+# \u = username
+# \h = ShortHostname
+# \d = date
+# \t = Timme 24h, with second
+# \? = Exitstatus
+# \w = working directory
+# \W = Working directory base name
